ef3591ced3367d2fa1da0474ef295207a875d34f
Wicus Buys <wicusbuys@gmail.com>
2024-06-30 21:10:19 +0200

    Challenge 18 upload

---
+++ C18_theReplicatorOfDTo/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] originalArray = new int[5];
+
+Console.WriteLine("Enter 5 numbers.");
+
+for (int i = 0; i < originalArray.Length; i++)
+{
+    Console.Write($"Number {i+1}: ");
+    originalArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int[] copiedArray = new int[5];
+
+for (int i = 0; i < originalArray.Length; i++)
+{
+    copiedArray[i] = originalArray[i];
+}
+
+for (int i = 0; i < originalArray.Length; i++)
+{
+    Console.WriteLine($"Original array: {originalArray[i],-20} Copied array: {copiedArray[i]}");
+}
