d6761ff86de875530389aad3a2c0096bf5ec1ea4
Wicus Buys <wicusbuys@gmail.com>
2024-06-30 21:34:27 +0200

    Challenge 19 upload

---
+++ C19_theLawsOfFreach/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int currentSmallest = int.MaxValue;
+int total = 0;
+
+foreach (int num in array)
+{
+    if (num < currentSmallest)
+    currentSmallest = num;
+
+    total += num;
+}
+
+float average = (float)total / array.Length;
+Console.WriteLine($"The average is {average} and the smallest number is {currentSmallest}");
