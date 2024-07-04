cdadedc1ae0de9c612b593b971f378e5554c29d5
Wicus Buys <wicusbuys@gmail.com>
2024-07-04 22:14:09 +0200

    Challenge 21 upload

---
+++ C21_countdown/Program.cs
@@ -0,0 +1,20 @@
+﻿//Challenge: Countdown
+//-----------------------------------------
+int number;
+
+do
+{
+    Console.WriteLine("Enter a number to countdown from");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+while (number <= 0);
+
+Countdown(number);
+
+ void Countdown(int number)
+ {
+    if (number == 0) return;
+
+    Console.WriteLine(number);
+    Countdown(number - 1);
+ }
