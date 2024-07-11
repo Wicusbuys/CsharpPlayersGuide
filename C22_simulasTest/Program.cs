51d63a154f66a2746cabc0bfd433917473b4b6e1
Wicus Buys <wicusbuys@gmail.com>
2024-07-11 22:01:05 +0200

    Challenge 22 upload

---
+++ C22_simulasTest/Program.cs
@@ -0,0 +1,18 @@
+﻿//Challenge: Simula's Test
+//--------------------------------------------------
+
+ChestState state = ChestState.Locked;
+
+while (true)
+{
+    Console.Write($"The chest is {state}. What do you want to do? ");
+
+    string input = Console.ReadLine();
+
+    if (state == ChestState.Locked && input == "unlock") state = ChestState.Closed;
+    if (state == ChestState.Closed && input == "open") state = ChestState.Open;
+    if (state == ChestState.Open && input == "close") state = ChestState.Closed;
+    if (state == ChestState.Closed && input == "lock") state = ChestState.Locked;
+}
+
+enum ChestState { Open, Closed, Locked }
